d8b5035d8343f0e7f127d81479ed3959a9a20dce
Nikita Strygin <nikita6@bk.ru>
2022-11-24 02:58:03 +0300

    Build only the plugin on windows

--- plugin/recv.bat
+++ plugin/recv.bat
@@ -1,5 +1,5 @@
 REM I HATE WINDOWS I HATE WINDOWS I HATE WINDOWS
 cd /d %~dp0
 tar -xf -
-cargo b --release
+cargo b --release --package mangai-clean-ps-plugin
 copy /y "target\release\mangai_clean_ps_plugin.dll" "C:\Program Files\Adobe\Adobe Photoshop 2022\Plug-ins\mangai_clean.8bf"
